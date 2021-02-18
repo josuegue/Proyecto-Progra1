e74207d37eb15e78ede41e3e05ed8e6842c93598
josueguevara2 <jguevaral1@miumg.edu.gt>
2021-02-18 17:09:04 -0600

    Ejercicio que determina si un caracter es mayuscula

---
+++ sumanumeros/sumanumeros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sumanumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra;
+            Console.Write("Ingrese letra  ");
+            letra = Convert.ToChar(Console.ReadLine());
+
+            
+            if (letra>=65 && letra<=90)
+            {
+                Console.WriteLine("La letra " + letra + " es Mayuscula");
+            }
+            else
+            {
+                Console.WriteLine("Es minuscula la letra");
+            }
+        }
+    }
+}
