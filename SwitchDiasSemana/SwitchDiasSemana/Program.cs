ec079bead2c8ec98db27534054e830b232e9dccb
josueguevara2 <jguevaral1@miumg.edu.gt>
2021-02-18 17:29:15 -0600

    Programa que determina si un dia de la semana es laboral o no.

---
+++ SwitchDiasSemana/SwitchDiasSemana/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace SwitchDiasSemana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+            Console.Write("Ingrese Dia de la semana:  ");
+            opcion =  Console.ReadLine();
+            opcion = (CultureInfo.InvariantCulture.TextInfo.ToTitleCase(opcion));
+            switch (opcion)
+            {
+                case "Lunes":Console.WriteLine("Dia Laboral");
+                    break;
+                case "Martes":Console.WriteLine("Dia Laboral");
+                    break;
+                case "Miercoles":Console.WriteLine("Dia Laboral");
+                    break;
+                case "Jueves":Console.WriteLine("Dia Laboral");
+                    break;
+                case "Viernes":Console.WriteLine("Dia Laboral");
+                    break;
+                case "Sabado":Console.WriteLine("Dia No Laboral");
+                    break;
+                case "Domingo":Console.WriteLine("Dia No Laboral");
+                    break;
+                default:
+                    Console.WriteLine("Error....Ingrese un dia de la semana");
+                    break;
+
+
+
+            }
+        }
+    }
+}
