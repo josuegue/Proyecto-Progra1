ae99519c97037295badf340cdafd8e9d2c167cb2
josueguevara2 <jguevaral1@miumg.edu.gt>
2021-02-18 17:25:04 -0600

    Es programa determina si un año es biciesto o no.

---
+++ Añobisiesto/Añobisiesto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Añobisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio = 0;
+            
+            Console.Write("Ingrese año_____");
+            anio = int.Parse(Console.ReadLine());
+
+            if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)){
+                Console.WriteLine("El año " + anio + " es biciesto");
+            }
+            else
+            {
+                Console.WriteLine("El año no es biciesto");
+            }
+        }
+    }
+}
