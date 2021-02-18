668568bbce819c97a820878d196464107eb06db1
josueguevara2 <jguevaral1@miumg.edu.gt>
2021-02-18 17:15:07 -0600

    Este programa determina un descuento del 20%

---
+++ ProgramaDescuento/ProgramaDescuento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProgramaDescuento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float total; 
+            float descuento;
+            Console.Write("Ingrese total: Q");
+            total = int.Parse(Console.ReadLine());
+            Console.WriteLine(total);
+            if (total > 300)
+            {
+                descuento = (total*80)/100;
+                Console.WriteLine("Precio total con descuento es Q." + descuento + ".00");
+            }
+            else 
+            {
+                Console.WriteLine("No hay descuento precio final de Q." + total + ".00");
+            }
+
+        }
+    }
+}
